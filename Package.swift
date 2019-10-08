90bef9b306262e616fa8785278a88933159ee39e
Kaan Dedeoglu <kaandedeoglu@me.com>
2019-10-08 15:23:15 +0200

    Update XcodeProj dependency to 7.1.0 (#11)

--- Package.swift
+++ Package.swift
@@ -7,12 +7,12 @@ let package = Package(
     name: "Shark",
     dependencies: [
         .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.1.0"),
-        .package(url: "https://github.com/tuist/xcodeproj.git", .upToNextMajor(from: "6.6.0")),
+        .package(url: "https://github.com/tuist/xcodeproj.git", .upToNextMajor(from: "7.1.0")),
     ],
     targets: [
         .target(
             name: "Shark",
-            dependencies: ["SPMUtility", "xcodeproj"]),
+            dependencies: ["SPMUtility", "XcodeProj"]),
         .testTarget(
             name: "SharkTests",
             dependencies: ["Shark"]),
